462e5f19d12ea6f59098433d5f6537859db04a88
Fakhar Mehdi <fakhar.mehdi@mergestack.com>
2023-06-20 17:32:10 +0500

    ⚡️ changed names and performed comditional rendering

--- src/Components/Homepage/index.js
+++ src/Components/Homepage/index.js
@@ -1,12 +1,11 @@
-
-import { DisplayBox } from '../DisplayBox';
+import { ToDoList } from '../ToDoList';
 
 export function Homepage ({handleChange, setTasks,    tasks, inputValue, handleKeyPress})
 {
   return(
     <div className="App">
       
-      <DisplayBox inputValue={inputValue} handleChange={handleChange} handleKeyPress={handleKeyPress} setTasks={setTasks} tasks={tasks}  />
+      <ToDoList inputValue={inputValue} handleChange={handleChange} handleKeyPress={handleKeyPress} setTasks={setTasks} tasks={tasks}  />
 
   </div>
   );
